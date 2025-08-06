86fa8f8768a5ee8b61e654ceaea67245590281e4
itaru2622 <itaru2622@gmail.com>
2025-08-06 16:03:11 +0900

    add feature: client.py gathers and outputs capabilities of MCP servers by calling list_xxx.

--- src/client.py
+++ src/client.py
@@ -19,6 +19,8 @@ import asyncio
 import logging
 from fastmcp.utilities.logging import get_logger
 from collections import defaultdict
+from fastapi.encoders import jsonable_encoder
+from itertools import cycle
 # my own ...
 from utils import load
 
@@ -26,6 +28,7 @@ from utils import load
 async def test(cli) -> Any:
 
     rtn = defaultdict(list[Any])
+    arr = []
 
     cmds = [
              ( 'tools', cli.list_tools )
@@ -37,10 +40,20 @@ async def test(cli) -> Any:
     async with cli:
       for k, op in cmds:
         tmp = await op()
-        print(f'{k} {tmp=}', file=sys.stderr)
+        arr += zip( cycle([k]), (tmp or []) ) # same as: arr += [ (k, v) for v in (tmp or []) ]
         rtn[k] = tmp
 
-    print(f'{rtn}', file=sys.stderr)
+    return rtn, arr
+
+
+def get_event_loop() -> Any:
+    '''get async event loop '''
+
+    #return asyncio.get_event_loop() # DeprecationWarning There is no current event loop
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop
 
 
 if __name__ == '__main__':
@@ -51,15 +64,28 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--log_level', help='MCP log level',                      default='DEBUG')
     opts = parser.parse_args()
 
-    for comp in [ "fastmcp.experimental.utilities.openapi.director",
-                  "fastmcp.experimental.server.openapi.components",
-                  "fastmcp.experimental.server.openapi.server" ]:
-        get_logger(comp).setLevel(logging.DEBUG)
-
+    #for comp in [ 'fastmcp.experimental.utilities.openapi.director',
+    #              'fastmcp.experimental.server.openapi.components',
+    #              'fastmcp.experimental.server.openapi.server' ]:
+    #    get_logger(comp).setLevel(logging.DEBUG)
+    #
     #logging.basicConfig(level=logging.DEBUG) # Configure root logger
 
     spec = load(opts.spec)
     client = Client(spec)
     #exit(0)
 
-    asyncio.run( test(client) )
+    #asyncio.run( test(client) )
+    loop = get_event_loop()
+    _, tmp = loop.run_until_complete(asyncio.gather( test(client) ))[0]
+    loop.close()
+
+    # reshape responded data to make them easy to use.
+    rtn = []
+    for _,v in tmp:
+       d = v.model_dump(); # base data: pydantic BaseModel dump
+       T = type(v)
+       d.update({  'type': T.__name__, 'class': f'{T.__module__}.{T.__qualname__}' }) # add type info
+       rtn.append(d)
+
+    print(json.dumps(rtn, ensure_ascii=False, indent=2), file=sys.stdout)
